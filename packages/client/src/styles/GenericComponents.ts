152158a2101ffdc40bb537c2b1a99009378d2b89
Georgi Raykovski <raykovski.georgi@gmail.com>
2023-12-09 23:02:07 +0200

    Add hover states to input and button components

--- packages/client/src/styles/GenericComponents.ts
+++ packages/client/src/styles/GenericComponents.ts
@@ -4,11 +4,19 @@ export const Input = styled.input`
   padding: 12px;
   border-radius: 8px;
   border: 0.5px solid rgba(0, 0, 0, 0.2);
+  transition: border-color 0.5s ease;
 
   &::placeholder {
-    color: rgba(0, 0, 0, 0.5);
+    color: rgba(0, 0, 0, 0.7);
+  }
+
+  &:focus {
+    outline: none;
+    border-color: #ffa511; /* Adjust color to match your design */
+    box-shadow: 0 0 5px rgba(255, 165, 17, 0.5); /* Optional: Add a subtle box-shadow */
   }
 `;
+
 export const Button = styled.button`
   font-size: 1rem;
   padding: 12px 0;
@@ -17,6 +25,12 @@ export const Button = styled.button`
   border-width: 0;
   color: #fff;
   cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #e59400;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
 `;
 
 export const FlexContainer = styled.div`

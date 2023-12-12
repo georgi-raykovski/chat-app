74c0d06743c64ac723c878e56773309179c43cab
Georgi Raykovski <raykovski.georgi@gmail.com>
2023-12-12 23:34:13 +0200

    Fix bug with theme provider

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { LoginForm, Chat } from './components';
-import { useTheme, useAuth } from './hooks';
+import { LoginForm, Chat, MainContainer } from './components';
+import { useAuth, useTheme } from './hooks';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './styles';
 
 const App = () => {
   const { isUserLoggedIn, login, logout } = useAuth();
-  const { onThemeSwitchHandler } = useTheme();
+  const { currentTheme, onThemeSwitchHandler } = useTheme();
 
   return (
-    <>
-      {!isUserLoggedIn && <LoginForm loginClickHandler={login} switchTheme={onThemeSwitchHandler} />}
-      {isUserLoggedIn && <Chat logoutClickHandler={logout} switchTheme={onThemeSwitchHandler} />}
-    </>
+    <ThemeProvider theme={currentTheme}>
+      <MainContainer>
+        <GlobalStyles />
+        {!isUserLoggedIn && <LoginForm loginClickHandler={login} switchTheme={onThemeSwitchHandler} />}
+        {isUserLoggedIn && <Chat logoutClickHandler={logout} switchTheme={onThemeSwitchHandler} />}
+      </MainContainer>
+    </ThemeProvider>
   );
 };
 
--- packages/client/src/AppProviders.tsx
+++ packages/client/src/AppProviders.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { MainContainer, MessagesProvider, SocketProvider, UsernameProvider } from './components';
-import { GlobalStyles } from './styles';
-import { useTheme } from './hooks';
+import { MessagesProvider, SocketProvider, UsernameProvider } from './components';
 
 export const AppProviders = ({ children }: { children: React.ReactNode }) => {
-  const { currentTheme } = useTheme();
-
   return (
-    <ThemeProvider theme={currentTheme}>
-      <UsernameProvider>
-        <SocketProvider>
-          <MessagesProvider>
-            <MainContainer>
-              <GlobalStyles />
-              {children}
-            </MainContainer>
-          </MessagesProvider>
-        </SocketProvider>
-      </UsernameProvider>
-    </ThemeProvider>
+    <UsernameProvider>
+      <SocketProvider>
+        <MessagesProvider>{children}</MessagesProvider>
+      </SocketProvider>
+    </UsernameProvider>
   );
 };
